c3516feef4b2e0df29c85d4bea7c51ddc5013b6e
Nepalcem <mark.emailtest@gmail.com>
2023-06-18 11:25:26 +0300

    persist delete

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,39 +1,19 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import { contactsSlice } from './slices/contactsSlice';
 import { filterSlice } from './slices/filterSlice';
-import storage from 'redux-persist/lib/storage';
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist';
 
-const contactsPersistConfig = {
-  key: 'contacts',
-  storage: storage,
-  blacklist: ['filter'],
-};
+
+
 
 const rootReducer = combineReducers({
   contacts: contactsSlice.reducer,
   filter: filterSlice.reducer,
 });
 
-const persistedReducer = persistReducer(contactsPersistConfig, rootReducer);
+
 
 export const store = configureStore({
-  reducer: persistedReducer,
-  middleware: getDefaultMiddleware =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
+  reducer: rootReducer,
 });
 
-export const persistor = persistStore(store);
+
