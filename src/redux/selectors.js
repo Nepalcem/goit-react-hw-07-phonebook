a1e690b031e6876faa357d9d1ebfe33a10eb8444
Nepalcem <mark.emailtest@gmail.com>
2023-06-24 01:01:01 +0300

    getVisibleContacts memoized

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,13 +1,16 @@
+import { createSelector } from '@reduxjs/toolkit';
+
 export const getContacts = state => state.contacts.items;
 export const getIsLoading = state => state.contacts.isLoading;
 export const getError = state => state.contacts.error;
 export const getFilter = state => state.filter;
 
-export const getVisibleContacts = state => {
-  const contacts = getContacts(state);
-  const filterValue = getFilter(state);
-  const lowerCaseFilterValue = filterValue.toLowerCase();
-  return contacts.filter(contact =>
-    contact.name.toLowerCase().includes(lowerCaseFilterValue)
-  );
-};
+export const getVisibleContacts = createSelector(
+  [getContacts, getFilter],
+  (contacts, filterValue) => {
+    const lowerCaseFilterValue = filterValue.toLowerCase();
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(lowerCaseFilterValue)
+    );
+  }
+);
